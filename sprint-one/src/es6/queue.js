373847fe3a354337b1e7bce11b530d552e8d96de
Michael Eskarous <mneskarous@gmail.com>
2019-10-24 15:25:04 -0700

    Implemented Queue class and methods in ES6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,5 +1,23 @@
 class Queue {
   constructor() {
+    this._storage = {};
+    this._start = 0;
+    this._end = 0;
   }
 
+  enqueue(value) {
+    this._storage[this._end] = value;
+    this._end++;
+  }
+
+  dequeue() {
+    let dequeued = this._storage[this._start];
+    delete this._storage[this._start];
+    this.size() && this._start++;
+    return dequeued;
+  }
+
+  size() {
+    return this._end - this._start;
+  }
 }
