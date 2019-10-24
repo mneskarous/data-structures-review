6778a85e27be52ddabdc0b0809b9322977e46268
Michael Eskarous <mneskarous@gmail.com>
2019-10-24 15:18:53 -0700

    Implemented Stack class and methods

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,5 +1,20 @@
 var Stack = function() {
+  this._storage = {};
+  this._size = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this._storage[this._size] = value;
+  this._size++;
+};
 
+Stack.prototype.pop = function() {
+  this._size && this._size--;
+  var popped = this._storage[this._size];
+  delete this._storage[this._size];
+  return popped;
+};
 
+Stack.prototype.size = function() {
+  return this._size;
+};
