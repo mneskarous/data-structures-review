b8f9053383d6160c103e0f1f092ebe1ff26666b8
Michael Eskarous <mneskarous@gmail.com>
2019-10-24 15:28:45 -0700

    Implemented Stack class and methods in ES6

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,5 +1,22 @@
 class Stack {
   constructor() {
+    this._storage = {};
+    this._size = 0;
   }
 
+  push(value) {
+    this._storage[this._size] = value;
+    this._size++;
+  }
+
+  pop() {
+    this._size && this._size--;
+    let popped = this._storage[this._size];
+    delete this._storage[this._size];
+    return popped;
+  }
+
+  size() {
+    return this._size;
+  }
 }
