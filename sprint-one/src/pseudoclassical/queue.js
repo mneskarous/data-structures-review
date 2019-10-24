156dde6a46affb16fc9ace4e888f08aa07e55cee
Michael Eskarous <mneskarous@gmail.com>
2019-10-24 15:11:44 -0700

    Implemented Queue class and methods

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,21 @@
 var Queue = function() {
-
+  this._storage = {};
+  this._start = 0;
+  this._end = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this._storage[this._end] = value;
+  this._end++;
+};
 
+Queue.prototype.dequeue = function() {
+  var dequeued = this._storage[this._start];
+  delete this._storage[this._start];
+  this.size() && this._start++;
+  return dequeued;
+};
 
+Queue.prototype.size = function() {
+  return this._end - this._start;
+};
