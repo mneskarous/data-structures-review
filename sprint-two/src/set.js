4f1f5124554a91645734444d20e0a8354d4822ff
Michael Eskarous <mneskarous@gmail.com>
2019-10-25 09:51:05 -0700

    Implemented Set class

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,12 +1,13 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  
 };
 
 setPrototype.contains = function(item) {
