9195b81763054cfbf61cac6e7544f8dde43c6e8a
Michael Eskarous <mneskarous@gmail.com>
2019-10-25 09:36:20 -0700

    Implemented hasEdge method

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -33,6 +33,13 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // if the graph does not contain the from node
+  if (!this.contains(fromNode)) {
+    // return false
+    return false;
+  }
+  // otherwise, return the boolean value of the connection between the from node and the to node
+  return !!this.storage[fromNode].edges[toNode];
 };
 
 // Connects two nodes in a graph by adding an edge between them.
