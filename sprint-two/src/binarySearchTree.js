38fcb798751a1f4c63144b660420c5da495c8318
Michael Eskarous <mneskarous@gmail.com>
2019-10-25 11:56:19 -0700

    Implemented insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -11,6 +11,28 @@ var BinarySearchTree = function(value) {
 var binaryTreeMethods = {};
 
 binaryTreeMethods.insert = function(val) {
+  // if the input is less than the node's value
+  if (val < this.value) {
+    // if the left child's value is null
+    if (this.left === null) {
+      // create a node with the input as the value and assign it to the left child
+      this.left = BinarySearchTree(val);
+    } else { // else 
+      // assign the input to the left child's value
+      this.left.insert(val);
+    }
+  } else if (val > this.value) { // else if the input is greater than the node's value)
+    // if the right child's value is null
+    if (this.right === null) {
+      // create a node with the input as the value and assign it to the right child
+      this.right = BinarySearchTree(val);
+    } else { // else
+      // assign the input to the right child's value
+      this.right.insert(val);
+    }
+  } else { // else
+    // do nothing
+  }
   
 };
 
